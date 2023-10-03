8a962703d22aff9dc2d7eb2ba4c9aa97151c3ff4
alex Parry <alexanderparry1976@outlook.com>
2023-10-03 05:16:30 +0000

    wtf2

--- Hardware/table.py
+++ Hardware/table.py
@@ -1,25 +1,25 @@
 from Hardware import db
 
 
-class Selection(db.table):
+class Category(db.Model):
     # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
-    Selection = db.relationship("selection", backref="Hardware", cascade="all, delete", lazy=True)
+    tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
         return self.category_name
 
 
-class Task(db.table):
+class Task(db.Model):
     # schema for the Task model
     id = db.Column(db.Integer, primary_key=True)
-    Hardware_name = db.Column(db.String(50), unique=True, nullable=False)
-    Hardware_description = db.Column(db.Text, nullable=False)
+    task_name = db.Column(db.String(50), unique=True, nullable=False)
+    task_description = db.Column(db.Text, nullable=False)
     is_urgent = db.Column(db.Boolean, default=False, nullable=False)
     due_date = db.Column(db.Date, nullable=False)
-    Hardware_id = db.Column(db.Integer, db.ForeignKey("Hardware.id", ondelete="CASCADE"), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
